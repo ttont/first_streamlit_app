48af9652c5910ab4cb9b75c6e9854820e580e79a
ttont <145419652+ttont@users.noreply.github.com>
2023-09-18 21:56:02 -0700

    Update streamlit_app.py
    add more items and emojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,8 @@
 import streamlit
 streamlit.title ('My Parents New Healthy Diner')
 streamlit.header ('Breakfast Menu')
-streamlit.text ('Connors Spam & Eggs')
-streamlit.text ('Omega 3 & Blueberry Oatmeal')
-streamlit.text ('Kale, Spinach & Rocket Smoothie')
-streamlit.text ('Hard-Boiled Free-Range Egg')
+streamlit.text ('🍳 Connors Spam & Eggs')
+streamlit.text ('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text ('🥤 Kale, Spinach & Rocket Smoothie')
+streamlit.text ('🥚 Hard-Boiled Free-Range Egg')
+streamlit.text ('🥑🍞 Avocado Toast')
